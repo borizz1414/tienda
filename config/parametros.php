b83c59b87a49c1a2226de8968d3db96e225b48ea
borizz1414 <41527903+borizz1414@users.noreply.github.com>
2019-04-21 20:57:50 -0400

    Update parametros.php

--- config/parametros.php
+++ config/parametros.php
@@ -1,6 +1,6 @@
 <?php 
 
-define("base_url", "https://github.com/borizz1414/");
+define("base_url", "https://github.com/borizz1414/tienda");
 define("controller_default", "productoController");
 define("action_default", "index");
 ?>
